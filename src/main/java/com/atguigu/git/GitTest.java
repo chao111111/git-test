f61e1985409f5f2f526a94a865b560924e9ff76e
chao111111 <503003376@qq.com>
2022-06-16 09:02:56 +0800

    git commit4

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("gitee test!");
         System.out.println("gitee test!  xinzeng !!");
         System.out.println("gitee test!  xinzeng !!");
+        System.out.println("push  新增 !!");
 }
 }
