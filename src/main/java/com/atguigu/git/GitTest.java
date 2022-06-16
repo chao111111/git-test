32ba57e9403b2d2f3525fb83a474afc9100e390e
chao111111 <503003376@qq.com>
2022-06-16 08:48:34 +0800

    gitee commit3

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,5 @@ public class GitTest {
         System.out.println("gitee test!");
         System.out.println("gitee test!  xinzeng !!");
         System.out.println("gitee test!  xinzeng !!");
-    }
+}
 }
